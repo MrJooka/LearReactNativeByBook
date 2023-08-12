edd3ea992117977fc9c96d92a8c88cf1c764883e
MrJooka <mr.jooka@gmail.com>
2023-08-13 05:53:46 +0900

    2.3 컴포넌트에 defaultProps 생성하는 방법

--- components/Greeting.tsx
+++ components/Greeting.tsx
@@ -6,11 +6,14 @@ interface Props {}
 const Greeting = (props: Props) => {
   return (
     <View>
-      <Text>Greeting</Text>
+      <Text>{props.name} 안녕하세요</Text>
     </View>
   );
 };
 
 export default Greeting;
+
+// 개인적으로 굳이 defaultProps를 사용해야할까? 그냥 props를 넘겨주는게 더 좋지 않을까?
+Greeting.defaultProps = {name: '주까님'};
 
 const styles = StyleSheet.create({});
